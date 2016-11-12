bec1cf2549f00ca60453e45bfb2a3607b6e8fa36
Loy van Beek <loy.vanbeek@gmail.com>
2016-11-12 16:22:19 +0100

    Starting to add tests that also help understanding the code

--- src/nl_robot_console/test/test_cfgparser.py
+++ src/nl_robot_console/test/test_cfgparser.py
@@ -1,7 +1,21 @@
 import unittest
 from ..cfgparser import CFGParser
+from ..cfgparser import parse_next_atom
 
 
-class TesCfgParser(unittest.TestCase):
-    def testAddRule(self):
+class TestCfgParser(unittest.TestCase):
+    def test_add_rule_1(self):
         parser = CFGParser()
+
+        self.assertEqual(len(parser.rules), 0)
+
+        parser.add_rule("""SIDE["left"] -> left""")
+
+        self.assertEqual(len(parser.rules), 1)
+
+    def test_parse_next_atom_1(self):
+        (name, semantics, remaining) = parse_next_atom("""SIDE["left"]""")
+
+        self.assertEqual(name, "SIDE")
+        self.assertEqual(semantics, '"left"')
+        self.assertEqual(remaining, '')
